6036b13d25b5ce2d523460fc95e7cc65523a7f1b
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-07-01 17:02:37 +0330

    genrate multiple numbers

--- src/randnum.c
+++ src/randnum.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-	printf("Hello World!");
-	return 0;
+int make_random_number() {
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+
+  srand(ts.tv_nsec);
+
+  return rand();
+}
+
+int main(int argc, char *argv[]) {
+  if (argc != 2 && argc != 1) {
+    fprintf(stderr, "Usage: %s <number of numbers>\n", argv[0]);
+  } else if (argc == 1) {
+    printf("%d\n", make_random_number());
+  } else {
+    for (int i = 0; i < atoi(argv[1]); i++) {
+      printf("%d\n", make_random_number());
+    }
+  }
+  return 0;
 }
