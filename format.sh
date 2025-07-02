df237a7d1ff6d1e163560c9d4410e0acf78142c4
mohammadmehdisharife <mohammadmehdisharife@gmail.com>
2025-07-02 14:15:01 +0330

    format code

--- format.sh
+++ format.sh
@@ -1,7 +1,7 @@
 # Script to format all C/C++ source files in the project using astyle
 
 # Style settings
-ASTYLE_OPTIONS="--style=google --indent=spaces=4 --suffix=none"
+ASTYLE_OPTIONS="--style=kr --indent=spaces=4 --suffix=none"
 
 # Find and format all relevant files
 find . \( -name "*.c" -o -name "*.h" -o -name "*.cpp" -o -name "*.hpp" \) -print0 | while IFS= read -r -d '' file; do
